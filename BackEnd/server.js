1189fbdd627a1f90700c72fc61adda4f30ecb172
Timmy Forde <G00403502@atu.ie>
2023-12-08 09:43:24 +0000

    first commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 4000
 const cors = require('cors');
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../build')));
+app.use('/static', express.static(path.join(__dirname, 'build//static')));
 
 app.use(cors());
 app.use(function(req, res, next) {
@@ -65,9 +68,7 @@ app.post('/api/book', (req,res)=>{
 
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
 
 app.get('/api/books', async(req, res)=>{
     
@@ -81,6 +82,11 @@ app.get('/api/book/:identifier',async (req,res)=>{
   let book = await bookModel.findById(req.params.identifier);
   res.send(book);
 })
+
+// Handles any requests that don't match the ones above
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/../build/index.html'));
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
